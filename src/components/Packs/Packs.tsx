63e3e3ea6d5d95e7b6570c9c6a9e9af8c6a5094d
Kseniya Smirnova <fanni@bk.ru>
2023-01-27 16:22:44 +0300

    feat: Packs: HTML structure

--- src/components/Packs/Packs.tsx
+++ src/components/Packs/Packs.tsx
@@ -6,20 +6,25 @@ import s from './Packs.module.css'
 
 
 export const Packs = () => {
-
   const isLoggedIn = useAppSelector((state) => state.login.isLoggedIn)
 
-
-if (!isLoggedIn) {
-    return <Navigate to={'/login'}/>
-}
+  if (!isLoggedIn) {
+    return <Navigate to={'/login'} />
+  }
   return (
-    <div className={s.wrapper}>  
+    <div className={s.wrapper}>
       <div className={s.header}>
-        
-        </div>  
-     
-       <div className={s.title}>Packs List</div>
+        <div className={s.btn}>
+          <button>Add new pack</button>
+        </div>
+
+        <div>search- </div>
+        <div>-Show packs cards- </div>
+        <div>-Number of cards- </div>
+        <div>-filter reset </div>
+      </div>
+
+      <div className={s.title}>Packs List</div>
       <PacksTable />
     </div>
   )
--- src/components/Packs/PacksTable/PacksTable.tsx
+++ src/components/Packs/PacksTable/PacksTable.tsx
@@ -1,100 +1,96 @@
-import React, { useEffect } from 'react';
-import { useAppDispatch, useAppSelector } from '../../../store/store';
-import { Pagination } from '../../Pagination/Pagination';
-import { getPacksTC, packsActions } from '../PacksBLL/packs-reducer';
-import { PacksList } from './PacksUI/PackList/PacksList';
+import React, { useEffect } from 'react'
+import { useAppDispatch, useAppSelector } from '../../../store/store'
+import { Pagination } from '../../Pagination/Pagination'
+import { getPacksTC, packsActions } from '../PacksBLL/packs-reducer'
+import { PacksList } from './PacksUI/PackList/PacksList'
 import s from './PacksUI/PacksTable/table.module.css'
 
-
-
-
-
-export const PacksTable=()=>{
-
+export const PacksTable = () => {
   const dispatch = useAppDispatch()
 
-  const cardPacks= useAppSelector(state => state.packs.cardPacks)
-  const packName = useAppSelector(state => state.packs.params.packName)
-  const user_id = useAppSelector(state => state.packs.params.user_id)
-  const sortPacks = useAppSelector(state => state.packs.params.sortPacks)
-  const min = useAppSelector(state => state.packs.params.min)
-  const max = useAppSelector(state => state.packs.params.max)
-  const cardPacksTotalCount= useAppSelector (state => state.packs.cardPacksTotalCount)
-  const page = useAppSelector(state => state.packs.params.page)
-  const pageCount = useAppSelector(state => state.packs.params.pageCount)
- 
-
-
+  const cardPacks = useAppSelector((state) => state.packs.cardPacks)
+  const packName = useAppSelector((state) => state.packs.params.packName)
+  const user_id = useAppSelector((state) => state.packs.params.user_id)
+  const sortPacks = useAppSelector((state) => state.packs.params.sortPacks)
+  const min = useAppSelector((state) => state.packs.params.min)
+  const max = useAppSelector((state) => state.packs.params.max)
+  const cardPacksTotalCount = useAppSelector(
+    (state) => state.packs.cardPacksTotalCount
+  )
+  const page = useAppSelector((state) => state.packs.params.page)
+  const pageCount = useAppSelector((state) => state.packs.params.pageCount)
 
   useEffect(() => {
- 
     dispatch(getPacksTC())
+  }, [dispatch, packName, user_id, sortPacks, min, max, pageCount])
 
-   
+  const onPageChanged = (page: number) => {
+    dispatch(packsActions.setCurrentPage(page))
+    dispatch(getPacksTC())
+  }
 
-}, [dispatch, packName, user_id, sortPacks, min, max, pageCount])
+  return (
+    <div className={s.tableContainer}>
+      <table className={s.table}>
+        <thead className={s.tableThead}>
+          <tr>
+            <th>
+              <div>
+                <div>Name</div>
+                <div className={s.triangle}>
+                  <div>▲</div>
+                  <div>▼</div>
+                </div>
+              </div>
+            </th>
+            <th>
+              <div>
+                <div>Cards</div>
+                <div className={s.triangle}>
+                  <div>▲</div>
+                  <div>▼</div>
+                </div>
+              </div>
+            </th>
+            <th>
+              <div>
+                <div>Updated</div>
+                <div className={s.triangle}>
+                  <div>▲</div>
+                  <div>▼</div>
+                </div>
+              </div>
+            </th>
+            <th>
+              <div>
+                <div>Creator</div>
+                <div className={s.triangle}>
+                  <div>▲</div>
+                  <div>▼</div>
+                </div>
+              </div>
+            </th>
+            <th>Action</th>
+          </tr>
+        </thead>
 
-const onPageChanged=(page:number)=>{
-  dispatch(packsActions.setCurrentPage(page))
-  dispatch(getPacksTC())
-}
+        <tbody>
+          <PacksList cardPacks={cardPacks} />
 
-  return <div className={s.tableContainer}>
- <table className={s.table}>
- <thead>
-  <tr>
- <th>
-        <div >
-            <div >Name</div>
-          
-        </div>
-    </th>
- <th>
-        <div >
-            <div >Cards</div>
-          
-        </div>
-    </th>
- <th>
-        <div >
-            <div >Updated</div>
-        
-        </div>
-    </th>
- <th>
-        <div >
-            <div >Creator</div>
-            {/* <div >
-                <div >▲</div>
-                <div>▼</div>
-            </div> */}
-        </div>
-    </th>
-    <th>
-      Action
-    </th>
-  </tr>
-  </thead>
- 
- <tbody>
- <PacksList cardPacks={cardPacks}/>
-       
-
-  <tr>
-   <td  colSpan={5}>
-     <div>
-    <Pagination
-       pageCount={pageCount}
-       cardPacksTotalCount={cardPacksTotalCount}
-       page={page}
-       onPageChanged={onPageChanged}
-      />
+          <tr>
+            <td colSpan={5}>
+              <div>
+                <Pagination
+                  pageCount={pageCount}
+                  cardPacksTotalCount={cardPacksTotalCount}
+                  page={page}
+                  onPageChanged={onPageChanged}
+                />
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-    </td>
-  </tr>
- </tbody>
-</table>
-</div>
+  )
 }
-
-
