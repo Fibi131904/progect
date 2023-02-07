9a788c0148394913565e95ed3c25bce99fc0aacc
Kseniya Smirnova <fanni@bk.ru>
2023-02-07 15:54:23 +0300

    fix:cardsList

--- src/components/Cards/CardsUI/Card.tsx
+++ src/components/Cards/CardsUI/Card.tsx
@@ -1,35 +1,40 @@
-import { Button } from "antd";
-import { FC } from "react"
-import {useParams} from 'react-router-dom'
-import { useAppSelector } from "../../../store/store";
-import { CardType } from "../CardsAPI/CardsAPI";
+import { Button } from 'antd'
+import { FC } from 'react'
+import { useParams } from 'react-router-dom'
+import { useAppSelector } from '../../../store/store'
+import { CardType } from '../CardsAPI/CardsAPI'
 
-
-type CardPropsType={
+type CardPropsType = {
   card: CardType
 }
 
-export const Card:FC<CardPropsType>=({card})=>{
-
-
-  const userId = useAppSelector((state) => state.profile.user._id)
-  const {packUserId} = useParams<'packUserId'>()
+export const Card: FC<CardPropsType> = ({ card }) => {
   
-const formatDate=(date: Date | string | number)=>{
-  return new Date(date).toLocaleDateString('ru-RU') + ' ' + new Date(date).toLocaleTimeString()
-}
+  const userId = useAppSelector((state) => state.profile.user._id)
+  const { packUserId } = useParams<'packUserId'>()
 
+  const formatDate = (date: Date | string | number) => {
+    return (
+      new Date(date).toLocaleDateString('ru-RU') +
+      ' ' +
+      new Date(date).toLocaleTimeString()
+    )
+  }
 
-  return <tr>
-        <td >{card.question}</td>
-        <td>{card.answer}</td>
-        <td>{formatDate(card.updated)}</td>
-        <td>{card.grade.toFixed(2)}</td>
-        {userId === packUserId && <td >
-        <div>
-         <Button>✎</Button>
-         <Button>✘</Button>
-        </div>
-        </td>}
+  return (
+    <tr>
+      <td>{card.question}</td>
+      <td>{card.answer}</td>
+      <td>{formatDate(card.updated)}</td>
+      <td>{card.grade.toFixed(2)}</td>
+      {userId === packUserId && (
+        <td>
+          <div>
+            <Button>✎</Button>
+            <Button>✘</Button>
+          </div>
+        </td>
+      )}
     </tr>
+  )
 }
--- src/components/Cards/CardsUI/CardsList.tsx
+++ src/components/Cards/CardsUI/CardsList.tsx
@@ -3,18 +3,16 @@ import { FC } from 'react'
 import { CardType } from '../CardsAPI/CardsAPI'
 import { Card } from './Card'
 
-
 type CardsListPropsType = {
   cards: CardType[]
 }
 
 export const CardsList: FC<CardsListPropsType> = ({ cards }) => {
-  
   return (
     <>
-      {cards.length? cards.map((card) => (
+      {cards.map((card) => (
         <Card key={card._id} card={card} />
-      )): <div>PACKS NOT FOUND</div>}
+      ))}
     </>
   )
 }
